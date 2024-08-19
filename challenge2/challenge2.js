133b1f606245275824371b72ec87f06af92f19b2
Brian Monestime <bmonestime@gmail.com>
2024-08-19 14:32:33 -0600

    challenge2 complete

---
+++ challenge2/challenge2.js
@@ -0,0 +1,8 @@
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+//Edabit Challenge:Very easy 
+
+function addition(num) {
+	var result = num +1
+
+    return result;
+}
