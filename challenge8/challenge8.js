f7d63f6f83800f6fe80a3a331bef7fbbae5e762b
Brian Monestime <bmonestime@gmail.com>
2024-08-29 21:48:28 -0600

    Convert minutes completed

---
+++ challenge8/challenge8.js
@@ -0,0 +1,10 @@
+//Write a function that takes an integer minutes and converts it to seconds.
+
+//return minutes * 60 
+
+//return the result
+
+function convert(minutes) {
+  return minutes * 60;
+}
+
